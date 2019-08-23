4aad4e91104a45f5461305eb4355c4aec9d5626a
Felix Boeseler <felix.boeseler@gmx.de>
2019-08-23 21:41:09 +0200

    Summation- Sicht funktioniert wieder

--- src/app/diary/components/add-entry-components/misc/food-intake-summation/food-intake-summation.component.ts
+++ src/app/diary/components/add-entry-components/misc/food-intake-summation/food-intake-summation.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { SettingsService } from 'src/shared/services/settings.service';
+import { FoodIntakeAttribute } from './../../../../../../shared/model/diary/entry/attributes/food-intake-attribute';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { IEntryFoodIntakeListPicker } from '../../inputs/interfaces/IEntryFoodIntakeListPicker';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-food-intake-summation',
   templateUrl: './food-intake-summation.component.html',
   styleUrls: ['./food-intake-summation.component.scss']
 })
-export class FoodIntakeSummationComponent implements OnInit {
+export class FoodIntakeSummationComponent implements OnInit, AfterViewInit {
 
-  @Input("foodIntakes") foodIntakes : IEntryFoodIntakeListPicker;
+  @Input("foodIntakes") foodIntakes: IEntryFoodIntakeListPicker;
 
-  constructor() { }
+  sum: Observable<number>;
+
+  constructor(private settings: SettingsService) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    this.sum = this.foodIntakes.foodArray.pipe(map((x: FoodIntakeAttribute[]) => x.map(x => x.amount ? x.amount * this.settings.carbsFactorSubj.getValue() : 0).reduce((x, y) => x + y)));
   }
 
 }
