0829ce46540bc31542539f644cda74725a069079
Felix Boeseler <felix.boeseler@gmx.de>
2019-08-24 16:19:05 +0200

    Beschreibung für Essen hinzugefügt

--- src/shared/components/food-editor/food-editor.component.ts
+++ src/shared/components/food-editor/food-editor.component.ts
@@ -26,17 +26,23 @@ export class FoodEditorComponent implements OnInit, IEntryFoodPicker {
     let name: string = null;
     let carbsFactor: number = null;
     let resorption: string = null;
+    let description: string = null;
     if (this.preselected) {
       this.foodInModification = this.preselected;
       name = this.preselected.name;
+      carbsFactor = this.preselected.carbsFactor? this.preselected.carbsFactor * 100 : null;
+      resorption = this.preselected.absorption === Absorption.FAST ? "f" : this.preselected.absorption === Absorption.MEDIUM ? "m" : this.preselected.absorption === Absorption.SLOW ? "s" : null;
+      description = this.preselected.description;
     }
     let nameControl: FormControl = this.fb.control(name, [Validators.maxLength(30)]);
     let carbsFactorControl: FormControl = this.fb.control(carbsFactor, [Validators.max(100), Validators.min(1)]);
-    let resorptionControl: FormControl = this.fb.control(carbsFactor, [Validators.max(100), Validators.min(1)]);
+    let resorptionControl: FormControl = this.fb.control(resorption, [Validators.max(100), Validators.min(1)]);
+    let descriptionControl: FormControl = this.fb.control(description, [Validators.max(3780), Validators.min(1)]);
     this.form.addControl("name", nameControl);
     this.form.addControl("carbsFactor", carbsFactorControl);
     this.form.addControl("resorption", resorptionControl);
-    this.form.setValidators([x => (nameControl.value || carbsFactorControl.value || resorptionControl.value) ? null : { "atLeastOneField": {} }]);
+    this.form.addControl("description", descriptionControl);
+    this.form.setValidators([x => (nameControl.value || carbsFactorControl.value || resorptionControl.value || descriptionControl.value) ? null : { "atLeastOneField": {} }]);
     this.form.valueChanges.subscribe(x => {
       this.foodInModification.name = nameControl.value ? nameControl.value : null;
       try {
@@ -44,6 +50,7 @@ export class FoodEditorComponent implements OnInit, IEntryFoodPicker {
       } catch (e) {
       }
       this.foodInModification.absorption = resorptionControl.value ? resorptionControl.value === 's' ? Absorption.SLOW : resorptionControl.value === 'm' ? Absorption.MEDIUM : Absorption.FAST : null;
+      this.foodInModification.description = descriptionControl.value ? descriptionControl.value : null;
       this.food.next(this.foodInModification);
     });
   }
