b34dcffb7b8874e755ff47aeaa8ad80604fe5b30
Felix Boeseler <felix.boeseler@gmx.de>
2019-09-01 20:50:00 +0200

    Zwischencommit

--- src/app/diary/components/add-entry-components/inputs/components/add-entry-intervall-food-bolus-picker/add-entry-intervall-food-bolus-picker.component.ts
+++ src/app/diary/components/add-entry-components/inputs/components/add-entry-intervall-food-bolus-picker/add-entry-intervall-food-bolus-picker.component.ts
@@ -1,93 +1,122 @@
 import { BaseInsulinIntakeSemantics } from './../../../../../../../shared/model/diary/entry/attributes/insulin-attribute';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
 import { IEntryIntervallInsulinIntakePicker } from '../../interfaces/IEntryIntervallInsulinIntakePicker';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { IntervallInsulinIntake } from 'src/shared/model/diary/entry/attributes/intervall-insulin-intake';
-import { FormBuilder, FormGroup, Validators, AbstractControl, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl, FormControl, NG_VALIDATORS, NG_VALUE_ACCESSOR, Validator, ControlValueAccessor } from '@angular/forms';
+import { ConstructionControlValue } from 'src/shared/util/construction-constrol-value';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-entry-intervall-food-bolus-picker',
   templateUrl: './add-entry-intervall-food-bolus-picker.component.html',
-  styleUrls: ['./add-entry-intervall-food-bolus-picker.component.scss']
+  styleUrls: ['./add-entry-intervall-food-bolus-picker.component.scss'],
+  providers: [
+    { provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => AddEntryIntervallFoodBolusPickerComponent), multi: true },
+    { provide: NG_VALIDATORS, useExisting: forwardRef(() => AddEntryIntervallFoodBolusPickerComponent), multi: true }]
 })
-export class AddEntryIntervallFoodBolusPickerComponent implements OnInit, IEntryIntervallInsulinIntakePicker {
+export class AddEntryIntervallFoodBolusPickerComponent implements OnInit, Validator, ControlValueAccessor {
 
-  @Input("group") group: FormGroup;
+  //CONTROLS
+  relativPart: FormControl
+  absolutePart: FormControl;
+  activated: FormControl;
+  time: FormControl;
+
+  //CONSTRUCTION
+  construction: Observable<ConstructionControlValue<IntervallInsulinIntake>>;
+
+  group: FormGroup = new FormGroup({});
   @Input("selectedBolusObservable") selectedBolusObservable: Observable<number>;
-
-  currentPreSelectedBolus: number = 0;
-  pickedIntake: BehaviorSubject<IntervallInsulinIntake> = new BehaviorSubject(null);
-  insulinAttribute: IntervallInsulinIntake;
-
-  intervallMinutes = 0;
-  relativeBolusPortion: number = 0;
-  activated = false;
-
-  relativePortionControl: FormControl;
-  kek: FormControl;
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.insulinAttribute = new IntervallInsulinIntake();
-    this.insulinAttribute.semanticIdentifier = BaseInsulinIntakeSemantics.FOOD_BOLUS;
-    this.insulinAttribute.insulin = null;
-
     this.selectedBolusObservable.subscribe(x => {
-      if (this.relativeBolusPortion > x) {
-        this.relativePortionControl.setValue(x ? x : 0);
+      let current = Number.parseInt(this.relativPart.value);
+      if (current > x) {
+        this.relativPart.setValue(x ? x : 0);
       }
-      this.currentPreSelectedBolus = x;
     });
 
-    this.relativePortionControl = this.fb.control('');
-    let relativePortionControl = this.relativePortionControl;
-    let fixPortionControl = this.fb.control('', [Validators.min(1), Validators.max(50)]);
-    let timeIntervallControl = this.fb.control('', [(x: AbstractControl) => !x.value && this.activated ? { 'timeIntervallNeedsTobeSet': {} } : null]);
-    this.kek = timeIntervallControl;
-    this.group.addControl('relativePortionControl', relativePortionControl);
-    this.group.addControl('fixPortionControl', fixPortionControl);
-    this.group.addControl('timeIntervallControl', timeIntervallControl);
-    this.group.setValidators([x => {
-      console.log("VALIDATE");
-      return this.activated && !relativePortionControl.value && !fixPortionControl.value ? { 'BolusNeedToBeSet': {} } : null
-    }]);
+    this.relativPart = this.fb.control(null);
+    this.absolutePart = this.fb.control('', [Validators.min(1), Validators.max(50)]);
+    this.time = this.fb.control('', [(x: AbstractControl) => !x.value && this.activated ? { 'timeIntervallNeedsTobeSet': {} } : null]);
+    this.activated = this.fb.control(false);
 
-    this.group.valueChanges.subscribe(x => {
+    this.group.addControl('relativePortionControl', this.relativPart);
+    this.group.addControl('fixPortionControl', this.absolutePart);
+    this.group.addControl('timeIntervallControl', this.time);
+    this.group.addControl('activated', this.activated);
 
+    this.construction = this.group.valueChanges.pipe(map(x => {
+      if (!this.activated.value) {
+        return null;
+      }
+      let constructed: IntervallInsulinIntake = new IntervallInsulinIntake();
+      constructed.semanticIdentifier = BaseInsulinIntakeSemantics.FOOD_BOLUS;
+      constructed.endTimeStamp = null;
+      constructed.units = null;
       try {
-        let sum = (relativePortionControl.value ? Number.parseInt(relativePortionControl.value) : 0) + (fixPortionControl.value ? Number.parseInt(fixPortionControl.value) : 0);
-        this.insulinAttribute.units = sum;
+        let sum = (this.relativPart.value ? Number.parseInt(this.relativPart.value.value) : 0) + (this.absolutePart.value ? Number.parseInt(this.absolutePart.value.value) : 0);
+        constructed.units = sum;
         let time = 0;
-        if (timeIntervallControl.value) {
-          let value: string = timeIntervallControl.value;
+        if (this.time.value) {
+          let value: string = this.time.value;
           time += value.split(':')[0] ? Number.parseInt(value.split(':')[0]) * 60 : 0;
           time += value.split(':')[1] ? Number.parseInt(value.split(':')[1]) : 0;
-          this.intervallMinutes = time;
         }
-        this.insulinAttribute.units = sum;
+        constructed.units = sum;
         let date = new Date();
         date.setMinutes(date.getMinutes() + time);
-        this.insulinAttribute.endTimeStamp = date;
-        this.pickedIntake.next(this.insulinAttribute);
-
-      } catch (e) {
-
-      }
-    });
-
-    relativePortionControl.valueChanges.subscribe(x => this.relativeBolusPortion = x ? Number.parseInt(x) : 0);
-
+        constructed.endTimeStamp = date;
+      } catch (e) { }
+      return new ConstructionControlValue(this.group.value, constructed);
+    }));
   }
 
-  onToggle() {
-    this.activated = !this.activated;
-    if (this.activated) {
-      this.pickedIntake.next(this.insulinAttribute);
-    } else {
-      this.pickedIntake.next(null);
+  writeValue(obj: any): void {
+    if (!obj) {
+      this.setToInitial();
+      return;
     }
-    this.group.setValue(this.group.value);
+    if (!(obj instanceof ConstructionControlValue)) {
+      throw new Error("Invalid Value");
+    } else if (obj.raw) {
+      this.group.setValue(obj.raw);
+    } else {
+      if (!obj.constructed) {
+        this.activated.setValue(false);
+        this.time.setValue(null);
+        this.relativPart.setValue(null);
+        this.absolutePart.setValue(null);
+      } else {
+        throw new Error("NOT IMPLEMENTED");
+      }
+    }
   }
+
+  private setToInitial() {
+    this.activated.setValue(false);
+    this.time.setValue(null);
+    this.relativPart.setValue(null);
+    this.absolutePart.setValue(null);
+  }
+
+  registerOnChange(fn: any): void {
+    this.construction.subscribe(fn);
+  }
+
+  registerOnTouched(fn: any): void {
+  }
+
+  validate(control: import("@angular/forms").AbstractControl): import("@angular/forms").ValidationErrors {
+    return this.group.valid ? null : { curruptedControlState: null };
+  }
+
+  registerOnValidatorChange?(fn: () => void): void {
+    this.group.statusChanges.subscribe(fn);
+  }
+
 
 }
