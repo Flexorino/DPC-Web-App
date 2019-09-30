db7eb89ba24c31e0895b91fc2646579c7008ba1c
Felix Boeseler <felix.boeseler@gmx.de>
2019-09-30 10:19:28 +0200

    zw

--- src/shared/effects/col-view-effects.ts
+++ src/shared/effects/col-view-effects.ts
@@ -1,3 +1,5 @@
+import { DiariesService } from './../../web-api/api/diaries.service';
+import { CompletableActionWithData } from './../actions/CompletableActionWitHData';
 import { UserManagementService } from './../../web-api/api/userManagement.service';
 import { CollViewComponent } from './../../app/configs/component/coll-view/coll-view.component';
 import { CollViewActions } from './../../app/configs/component/coll-view/coll-view.actions';
@@ -6,8 +8,8 @@ import { Actions } from '@ngrx/effects';
 import { EffectsUtil } from './effects-util';
 import { UserService } from '../services/user.service';
 import { CompletableAction } from '../actions/CompletableAction';
-import { Observable, timer } from 'rxjs';
-import { tap, map, take } from 'rxjs/operators';
+import { Observable, timer, EMPTY } from 'rxjs';
+import { tap, map, take, flatMap } from 'rxjs/operators';
 import { GeneralEffectActions } from './general-effect-actions';
 import { Patch } from '../services/patcherino/patch';
 import { Action } from '@ngrx/store';
@@ -17,14 +19,21 @@ import { DiaryReference } from '../model/user/diary-reference';
 export class CollViewEffects {
 
     private onOpened$;
+    private onAdded$;
+
     constructor(
-        private actions$: Actions, private util: EffectsUtil, private userService: UserService, private userManService: UserManagementService
+        private actions$: Actions, private util: EffectsUtil, private userService: UserService, private userManService: UserManagementService, private diaryService: DiariesService
     ) {
         this.onOpened$ = util.when(CollViewActions.OPENED).do(x => this.handleOpened(x));
+        this.onAdded$ = util.when(CollViewActions.DIARY_ADDED).do(x => this.handleAdded(x));
+    }
+
+    private handleAdded(props: CompletableActionWithData<CollViewComponent, void, { name: string }>): Observable<Action> {
+        return this.diaryService.addDiary({ preferences: { name: props.data.name } }).pipe(flatMap(x => this.handleOpened(props)));
     }
 
     private handleOpened(props: CompletableAction<CollViewComponent, void>): Observable<Action> {
-        return this.userManService.getUserDiaries().pipe(take(1), tap(x => props.resolve(null)), map(x => {
+        return this.userManService.getUserDiaries().pipe(tap(x => props.resolve(null)), map(x => {
             let ownedDiares = x.owned.map(x => new DiaryReference(x.id, x.preferences.name));
             return GeneralEffectActions.UserPatchReady({ patch: new Patch([], [{ myDiaries: ownedDiares }]) })
         }
