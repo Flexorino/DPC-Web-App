f4b0b86de3d7dbbba7bc728b3de9aa262624d831
Felix Boeseler <felix.boeseler@gmx.de>
2019-10-02 00:21:33 +0200

    Multisaft

--- src/shared/effects/effects-util.ts
+++ src/shared/effects/effects-util.ts
@@ -2,12 +2,12 @@
 import { Action, ActionCreator } from '@ngrx/store';
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { Observable, OperatorFunction } from 'rxjs';
-import { mergeMap } from 'rxjs/operators';
+import { Observable, OperatorFunction, forkJoin, EMPTY } from 'rxjs';
+import { mergeMap, map, flatMap } from 'rxjs/operators';
 import { TypedAction } from '@ngrx/store/src/models';
 
 export class Doable<T> {
-    public do: (handler: (x: T) => Observable<Action>) => Observable<Action>;
+    public do: (...handler: ((x: T) => Observable<Action>)[]) => Observable<Action>;
 }
 
 @Injectable({ providedIn: "root" })
@@ -16,12 +16,15 @@ export class EffectsUtil {
         private actions$: Actions
     ) { }
 
-    public  when<T extends object>(action: ActionCreator<any ,(props: T) => T & TypedAction<string>>): Doable<T> {
+    public when<T extends object>(action: ActionCreator<any, (props: T) => T & TypedAction<string>>): Doable<T> {
         let filter: OperatorFunction<Action, any> = ofType(action);
-        let doable : Doable<T> = {
-            do: (handler: (action: T) => Observable<Action>) => {
+        let doable: Doable<T> = {
+            do: (...handler: ((action: T) => Observable<Action>)[]) => {
                 let listener$ = createEffect(() => this.actions$.pipe(filter,
-                    mergeMap(x => handler(x))
+                    mergeMap(x => forkJoin(handler.map(z => z(x)))), mergeMap((x:any[]) => new Observable<any>(sub => {
+                        x.forEach(x => sub.next(x));
+                        sub.complete();
+                     }))
                 ));
                 return listener$;
             }
